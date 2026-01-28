05874829e0749f60c3271139259c67ad8046161f
calishu <danny.kretschmann@proton.me>
2026-01-28 15:44:01 +0100

    feat: adding debugging port for qemu and gdb

--- qemu.sh
+++ qemu.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 mkdir -p img/EFI/BOOT
+
 cp zig-out/bin/BOOTX64.efi img/EFI/BOOT/BOOTX64.EFI
+cp zig-out/bin/kernel img/kernel.bin
 
 qemu-system-x86_64 \
     -bios /usr/share/ovmf/x64/OVMF.4m.fd \
     -drive format=raw,file=fat:rw:img \
     -m 256M \
     -net none \
-    -serial stdio
+    -serial stdio \
+    -s -S
